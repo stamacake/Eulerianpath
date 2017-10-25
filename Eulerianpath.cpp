9370d2a1ce3a04cf73931eb39f65520bb4747940
stamacake <30477203+stamacake@users.noreply.github.com>
2017-10-26 02:36:27 +0400

    Update Eulerianpath.cpp

--- Eulerianpath.cpp
+++ Eulerianpath.cpp
@@ -15,19 +15,19 @@ using namespace std;
 
 int main() {
 	setlocale(0,"");
-	map<int, vector<int> > A1; //ключ - номера вершин, значения - номера вершин из которых можно
-							   //попасть в ключ
+	map<int, vector<int> > A1; //ГЄГ«ГѕГ· - Г­Г®Г¬ГҐГ°Г  ГўГҐГ°ГёГЁГ­, Г§Г­Г Г·ГҐГ­ГЁГї - Г­Г®Г¬ГҐГ°Г  ГўГҐГ°ГёГЁГ­ ГЁГ§ ГЄГ®ГІГ®Г°Г»Гµ Г¬Г®Г¦Г­Г®
+							   //ГЇГ®ГЇГ Г±ГІГј Гў ГЄГ«ГѕГ·
 	map<int, vector<int> > A2;
-	cout<<"введите кол-во вершин:";
+	cout<<"ГўГўГҐГ¤ГЁГІГҐ ГЄГ®Г«-ГўГ® ГўГҐГ°ГёГЁГ­:";
 	int v1 = 0;
 	cin>>v1;
 	for (int i = 1; i <= v1; i++) {
 		A2[i].push_back(0);
-		cout << "кол-во вершин идущих в вершину №" << i << " : ";
+		cout << "ГЄГ®Г«-ГўГ® ГўГҐГ°ГёГЁГ­ ГЁГ¤ГіГ№ГЁГµ Гў ГўГҐГ°ГёГЁГ­Гі В№" << i << " : ";
 		int v2 = 0;
 		cin >> v2;
 		for (int j = 1; j <= v2; j++) {
-			cout << "введите номер " << j << "-й вершины :";
+			cout << "ГўГўГҐГ¤ГЁГІГҐ Г­Г®Г¬ГҐГ° " << j << "-Г© ГўГҐГ°ГёГЁГ­Г» :";
 			int v3 = 0;
 			cin >> v3;
 			A1[i].push_back(v3);
@@ -36,8 +36,8 @@ int main() {
 	}
 	for (int i = 1; i <= v1; i++) {
 		int S = A1[i].size();
-		cout << "Вершина №" << i << "\n";
-		cout << "Смежные, ведущие к ней: ";
+		cout << "Г‚ГҐГ°ГёГЁГ­Г  В№" << i << "\n";
+		cout << "Г‘Г¬ГҐГ¦Г­Г»ГҐ, ГўГҐГ¤ГіГ№ГЁГҐ ГЄ Г­ГҐГ©: ";
 
 		for (int j = 0; j<S; j++) {
 
@@ -57,8 +57,8 @@ int main() {
 
 	for (int i = 1; i <= v1; i++) {
 		int S = A2[i].size();
-		cout << "Вершина №" << i << "\n";
-		cout << "Смежные, ведущие к ней: ";
+		cout << "Г‚ГҐГ°ГёГЁГ­Г  В№" << i << "\n";
+		cout << "Г‘Г¬ГҐГ¦Г­Г»ГҐ, ГўГҐГ¤ГіГ№ГЁГҐ ГЄ Г­ГҐГ©: ";
 
 		for (int j = 0; j<S; j++) {
 
@@ -84,7 +84,7 @@ int main() {
 		cout << "A3 last element	" << A3[0] << endl;
 		while (A3.size() != v1) {
 			int exit = -55;
-			cout << "путь А3  :";
+			cout << "ГЇГіГІГј ГЂ3  :";
 			for (auto aa : A3) {
 				cout << aa <<" ";
 			} cout << endl;
@@ -94,11 +94,12 @@ int main() {
 
 			int lastelement = A3[A3.size() - 1];
 			cout <<"last element: "<< lastelement << endl;
-			SizeTop = A1[lastelement].size(); // размер текущей вершины
+			SizeTop = A1[lastelement].size(); // Г°Г Г§Г¬ГҐГ° ГІГҐГЄГіГ№ГҐГ© ГўГҐГ°ГёГЁГ­Г»
 			cout << "size of top: " << SizeTop << endl;
 			while (exit != -5) {
 				
-				if ((find(A2[lastelement].begin(), A2[lastelement].end(), A1[lastelement][i1]) == A2[lastelement].end())&&(find(A3.begin(), A3.end(), A1[lastelement][i1]) == A3.end())) //проверка на присутствие элемента
+				if ((find(A2[lastelement].begin(), A2[lastelement].end(), A1[lastelement][i1]) == A2[lastelement].end())
+				    &&(find(A3.begin(), A3.end(), A1[lastelement][i1]) == A3.end())) //ГЇГ°Г®ГўГҐГ°ГЄГ  Г­Г  ГЇГ°ГЁГ±ГіГІГ±ГІГўГЁГҐ ГЅГ«ГҐГ¬ГҐГ­ГІГ 
 				{
 					
 					A2[lastelement].push_back(A1[lastelement][i1]);
